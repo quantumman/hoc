aa768410e09d170d390a213b57acec91780e3829
Shoadi Yokoyama <quantumcars@gmail.com>
2012-08-19 16:04:08 +0900

    Card: Add Playable type class

--- Card/Card.hs
+++ Card/Card.hs
@@ -1,5 +1,21 @@
+{-# LANGUAGE KindSignatures, DataKinds #-}
 
 -- | Card List
 data Card = FarmVillage
           | DaMaid
           | BurnAgriculture
+
+class Playable (c :: Card) where
+    cost :: Play (c :: Card) -> Int
+
+    coin :: Play (c :: Card) -> Int
+
+    cardType :: Play (c :: Card) -> CardType
+
+    subType :: Play (c :: Card) -> SubType
+
+    name :: Play (c :: Card) -> String
+    name = const ""
+
+    description :: Play (c :: Card) -> String
+    description = const ""
